788269d3ccec2c43d12f1d5c3601cc8fdaf13e13
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-24 01:02:51 +0900

    add new and add new components dire

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Header from '../components/header'
+
 const Home: NextPage = () => {
   return (
     <>
-        <h1 className='text-red-600'>Hello Wolrd</h1>
-        <p className='text-gray-800 text-xl'>このサイトはサンプルです！</p>
+      <Header />
     </>
 
   )
