0886c83a21990266b15c8ac276fdb1154c63897a
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-25 00:12:03 +0900

    add layout react icon s

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,8 +1,12 @@
+import { FaGithub } from "react-icons/fa"
+
 export default function Sidebar() {
     return (
         <>
-        <div className="w-full h-10 bg-gray-800 text-white">
+        <div className="w-full h-10">
             <h1 className="text-center p-2">Sidebar</h1>
+
+            <FaGithub />
         </div>
         </>
     )
