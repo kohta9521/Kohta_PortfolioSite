92402229b2b4b179feb82cf28a56d9c91d3e9cc1
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-24 23:43:42 +0900

    add components and index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Layout from '../components/layout'
+
 import Header from '../components/header'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Header />
+      <Layout>
+
+      </Layout>
+
     </>
 
   )
