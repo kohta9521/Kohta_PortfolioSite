cab7d5c466d91f128cf854bf66eb208d75d3cac5
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-25 15:13:28 +0900

    add acticity Image area and Next task is activity image include this area

--- components/activity.js
+++ components/activity.js
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+
 export default function Activity() {
     return (
         <>
             <h1>Here is Activity components</h1>
+            <div className='w-1/2 '>
+                <Image 
+                src=""
+                width="300px"
+                />
+            </div>
         </>
     )
 }
