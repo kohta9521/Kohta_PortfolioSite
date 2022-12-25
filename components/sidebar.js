6eed30842ed90b1cfee4a3e790777d74a20fc4fd
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-25 15:00:47 +0900

    add new sidebar tailwind

--- components/sidebar.js
+++ components/sidebar.js
@@ -3,20 +3,26 @@ import Image from 'next/image'
 export default function Sidebar() {
     return (
         <>
-            <div className='w-52 h-full bg-black'>
-                <div className='container'>
-                    <div className='w-full h-20'>
-                        <div className="h-16 w-full flex items-center">
-                            <Image
-                                        src="/enterlogo.png"
-                                        aria-label="Enter Logo"
-                                        role="img"
-                                        width="144"
-                                        height="30"
-                                        viewBox="0 0 144 30"
-                            />
+            <div className='w-full h-1/2 bg-black'>
+                <div className='w-full h-full'>
+                    <dir className='flex flex-no-wrap'>
+                        <div className='w-64 absolute'>
+                            <div className='px-8'>
+                                <div className='h-16 w-full flex items-cneter'>
+                                    <div className='px-8'>
+                                        <Image
+                                            src="/enterlogo.png"
+                                            aria-label="Enter Logo"
+                                            role="img"
+                                            width="144"
+                                            height="30"
+                                            viewBox="0 0 144 30"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </dir>
                 </div>
             </div>
         </>
