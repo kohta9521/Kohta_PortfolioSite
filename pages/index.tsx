befd23040d9c1c4bb8d2fde95d5e1c4c1eb6e2f3
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-25 15:04:37 +0900

    add profile components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
 import Sidebar from '../components/sidebar'
+import Profile from '../components/profile'
 
 const Home: NextPage = () => {
   return (
-    <Sidebar />
+    <>
+      <Sidebar />
+
+      <Profile />
+    </>
   )
 }
 
