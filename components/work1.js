0a4a5512400bc46373cb363808dd86d754943f87
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-27 01:51:42 +0900

    add work1 2

---
+++ components/work1.js
@@ -0,0 +1,9 @@
+export default function Work1() {
+    return (
+        <>
+            <div className="absolute right-4 top-28 w-1/5 h-1/5 bg-indigo-600 bg-opacity-20 rounded-lg">
+                <h1 className="text-2xl">work1</h1>
+            </div>
+        </>
+    )
+}
