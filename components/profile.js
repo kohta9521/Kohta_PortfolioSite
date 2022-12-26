2ac9c7f214b030c617b59d308bebfa247d0287d1
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-27 01:19:01 +0900

    add profile

--- components/profile.js
+++ components/profile.js
@@ -1,8 +1,8 @@
 export default function Profile() {
     return (
         <>
-        <div className="w-full h-auto bg-gray-500">
-            <h1>Profile</h1>
+        <div className="absolute t-100 w-500 h-20 bg-gray-500">
+            <h1 className="text-2xl">Profile</h1>
         </div>
         </>
     )
