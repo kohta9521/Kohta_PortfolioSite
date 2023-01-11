9b0f049ad324560f7afb8c64b4be45ab9da488ac
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-11 11:59:27 +0900

    remake webisaite remove all components filesd

--- components/profile.js
+++
@@ -1,9 +0,0 @@
-export default function Profile() {
-    return (
-        <>
-        <div className="absolute left-40 top-28 w-2/6 h-2/5 bg-indigo-600 bg-opacity-20 rounded-lg">
-            <h1 className="text-2xl">Profile</h1>
-        </div>
-        </>
-    )
-}
